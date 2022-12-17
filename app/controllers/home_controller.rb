d8db1427313b110864facbe295b22e4b6c430f5c
Arbin <sominyadav7@gmail.com>
2022-12-17 14:46:15 +0545

    project setup

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def task 
+  end
+
+  def tasks 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/about'
+  get 'home/task'
+  get 'home/tasks'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
